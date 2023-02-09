1d59e3b072c6cb7f995526beda60f66a1047cc04
penagarciaeduardo <penagarciaeduardo@gmail.com>
2023-02-09 10:39:52 +0000

    AudioScripting

--- Assets/Scripts/Edu/InGameMenu.cs
+++ Assets/Scripts/Edu/InGameMenu.cs
@@ -1,23 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InGameMenu : MonoBehaviour
 {
-    public GameObject menu;
-    public GameObject ajustes;
-    public void Continuar()
-    {
-        menu.SetActive(false);
-    }
-
-    public void Ajustes()
-    {
-        ajustes.SetActive(true);
-    }
-
     public void Salir()
     {
-        menu.SetActive(false);
+
     }
 }
