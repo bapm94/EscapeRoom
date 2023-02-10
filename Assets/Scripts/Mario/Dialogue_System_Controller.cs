b5365ff9b8e45d017a26e945c8b59d469bf07540
skapeek <msierra.siete@gmail.com>
2023-02-10 12:35:09 +0000

    avance dialogo

--- Assets/Scripts/Mario/Dialogue_System_Controller.cs
+++ Assets/Scripts/Mario/Dialogue_System_Controller.cs
@@ -10,32 +10,42 @@ public class Dialogue_System_Controller : MonoBehaviour
     public string[] sentences;
     int Index = 0;
     public float dialogueSpeed;
+    public float timeBetweenDialogues;
     bool dialogueOnGoing;
-    bool isTyping;
+    [SerializeField] bool isTyping;
+    int rangeMaxLocal;
+    [SerializeField] bool hasChangedSentence;
+    [SerializeField] bool coroutineRunning;
 
     void Start()
     {
+        coroutineRunning = false;
+        timeBetweenDialogues = 2.5f;
         dialogueSpeed = 0.05f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Keyboard.current.fKey.wasPressedThisFrame)
+        {
+            GetDialogueInfo(0, 3);
+        }
+
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
-            GetDialogueInfo(0, 1);
+            NextSentence(rangeMaxLocal);
         }
     }
 
     public void GetDialogueInfo(int rangeMin, int rangeMax)
     {
-        if (!isTyping)
-        {
-            if (!dialogueOnGoing)
-                StartDialogue(rangeMin);
-            else
-                NextSentence(rangeMax);
-        }
+        rangeMaxLocal = rangeMax;
+
+        if (!dialogueOnGoing)
+            StartDialogue(rangeMin);
+        else
+            NextSentence(rangeMaxLocal);        
     }
 
     void StartDialogue(int rangeMin)
@@ -48,7 +58,7 @@ public class Dialogue_System_Controller : MonoBehaviour
 
     void NextSentence(int rangeMax)
     {
-        if (dialogueOnGoing)
+        if (dialogueOnGoing && !isTyping)
         {
             if (Index > rangeMax)
             {
@@ -58,8 +68,26 @@ public class Dialogue_System_Controller : MonoBehaviour
             {
                 if (Index < sentences.Length)
                 {
+                    hasChangedSentence = true;
                     dialogueText.text = "";
                     StartCoroutine(WriteSentence());
+                }
+            }
+        }
+        else if (dialogueOnGoing && isTyping)
+        {
+            if (Index > rangeMax)
+            {
+                hasChangedSentence = true;
+                dialogueOnGoing = false;
+            }
+            else
+            {
+                if (Index < sentences.Length)
+                {
+                    hasChangedSentence = true;
+                    isTyping = false;
+                    dialogueText.text = sentences[Index];
                 }
             }
         }
@@ -68,13 +96,39 @@ public class Dialogue_System_Controller : MonoBehaviour
     IEnumerator WriteSentence()
     {
         isTyping = true;
-        foreach (char Character in sentences[Index].ToCharArray())
+        char[] Characters = sentences[Index].ToCharArray();
+        for (int i = 0; i < Characters.Length; i++)
+        {
+            if (i == 0)
+            {
+
+            }
+            else
+            {
+                dialogueText.text += Characters[i];
+                yield return new WaitForSeconds(dialogueSpeed);
+                if (!isTyping)
+                {
+                    hasChangedSentence = true;
+                    break;
+                }
+            }
+        }
+
+        /*foreach (char Character in Characters)
         {
             dialogueText.text += Character;
             yield return new WaitForSeconds(dialogueSpeed);
-        }
+            if (!isTyping)
+            {
+                hasChangedSentence = true;
+                break;
+            }
+        }*/
+
         Index++;
-        isTyping = false;
-        Debug.Log("HOLA");
+        isTyping = false; 
     }
+
+    //HOLA QUE TAL
 }
