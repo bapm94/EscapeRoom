7a35820a2abd2bcd52c7e69771fa4638c0afb676
skapeek <msierra.siete@gmail.com>
2023-02-28 10:52:49 +0000

    cposassdfadfgnhn

--- Assets/Scripts/Mario/Tutorial_Manager.cs
+++ Assets/Scripts/Mario/Tutorial_Manager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Tutorial_Manager : MonoBehaviour
 {
+    public TextMeshProUGUI tutorialText1;
+
     // Start is called before the first frame update
     void Start()
     {
