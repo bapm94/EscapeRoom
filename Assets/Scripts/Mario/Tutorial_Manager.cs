dd11851781b70a3baf4c531300641aa9afcad107
skapeek <msierra.siete@gmail.com>
2023-03-10 00:12:16 +0000

    mkklññk

--- Assets/Scripts/Mario/Tutorial_Manager.cs
+++ Assets/Scripts/Mario/Tutorial_Manager.cs
@@ -13,13 +13,17 @@ public class Tutorial_Manager : MonoBehaviour
     public GameObject wasd;
     public TextMeshProUGUI tuto2Text;
     public GameObject arrows;
-    public Sprite[] rotatingSpritesWASD;
-    public Sprite[] rotatingSpritesARROWS;
+    public Image[] rotatingSpritesWASD;
+    public Image[] rotatingSpritesARROWS;
     int currentSprite;
+    public GameObject trailTutorial3;
 
     private Controlls _controls;
     public Vector2 movement;
     public Vector2 rotation;
+
+    Vector3 initialPos;
+    bool stopTrail = true;
 
     delegate void ActivateTutorialsList();
 
@@ -34,7 +38,8 @@ public class Tutorial_Manager : MonoBehaviour
 
         _controls = new Controlls();
         _controls.CharacterControl.Enable();
-        Main_Character_Controller_v2.instance.canRotate = false;
+        Main_Character_Controller_v2.instance.canMove = false;
+        Bob_Controller.instance.HeadBob();
 
         if (Main_Game_Manager.instance != null)
         {
@@ -47,8 +52,8 @@ public class Tutorial_Manager : MonoBehaviour
                 tempVect1 = new Vector3(0, 0.175f, 0);
                 tutorialText.text = "Interact with this object!"; ;
                 RotateSprites();
-                wasd.SetActive(true);
-                arrows.SetActive(false);
+                arrows.SetActive(true);
+                wasd.SetActive(false);
 
                 tutorials.Add(StartTutorial3);
                 tutorials.Add(StartTutorial1);
@@ -61,8 +66,8 @@ public class Tutorial_Manager : MonoBehaviour
     {
         movement = _controls.CharacterControl.Walk.ReadValue<Vector2>() * Time.deltaTime;
         rotation = _controls.CharacterControl.Cam_Rotation.ReadValue<Vector2>() * Time.deltaTime;
-        if (!Main_Game_Manager.instance.movedForWasd && (Mathf.Abs(movement.normalized.x) > 0 || Mathf.Abs(movement.normalized.y) > 0)) { Main_Game_Manager.instance.movedForWasd = true; StartCoroutine(WasdCompleted()); }
-        if (Main_Game_Manager.instance.movedForWasd && !Main_Game_Manager.instance.lookedForArrows && (Mathf.Abs(rotation.normalized.x) > 0 || Mathf.Abs(rotation.normalized.y) > 0)) { Main_Game_Manager.instance.lookedForArrows = true; StartCoroutine(ArrowsCompleted()); }
+        if (!Main_Game_Manager.instance.movedForWasd && (Mathf.Abs(rotation.normalized.x) > 0 || Mathf.Abs(rotation.normalized.y) > 0)) { Main_Game_Manager.instance.movedForWasd = true; StartCoroutine(WasdCompleted()); }
+        if (Main_Game_Manager.instance.movedForWasd && !Main_Game_Manager.instance.lookedForArrows && (Mathf.Abs(movement.normalized.x) > 0 || Mathf.Abs(movement.normalized.y) > 0)) { Main_Game_Manager.instance.lookedForArrows = true; StartCoroutine(ArrowsCompleted()); }
 
         Camera camera = Camera.main;
         tutorialCanvas[0].transform.LookAt(tutorialCanvas[0].transform.position + camera.GetComponent<Camera>().transform.rotation * Vector3.forward, camera.GetComponent<Camera>().transform.rotation * Vector3.up);
@@ -113,8 +118,11 @@ public class Tutorial_Manager : MonoBehaviour
     #region Tutorial2
     public void RotateSprites()
     {
-        wasd.GetComponent<Image>().sprite = rotatingSpritesWASD[currentSprite];
-        arrows.GetComponent<Image>().sprite = rotatingSpritesARROWS[currentSprite];
+        for (int i = 0; i < 4; i++)
+        {
+            if (i == currentSprite) { rotatingSpritesWASD[i].color = new Color(0.47f, 0, 0, 1); rotatingSpritesARROWS[i].color = new Color(0.47f, 0, 0, 1); }
+            else { rotatingSpritesWASD[i].color = Color.black; rotatingSpritesARROWS[i].color = Color.black; }
+        }
         if (currentSprite < 3) { currentSprite++; }
         else { currentSprite = 0; }
         Invoke("RotateSprites", 0.66f);
@@ -124,17 +132,17 @@ public class Tutorial_Manager : MonoBehaviour
     {
         tuto2Text.text = "Great!";
         yield return new WaitForSeconds(1.0f);
-        Main_Character_Controller_v2.instance.canRotate = true;
-        tuto2Text.text = "To Look!";
-        wasd.SetActive(false);
-        arrows.SetActive(true);
+        Main_Character_Controller_v2.instance.canMove = true;
+        tuto2Text.text = "To Move!";
+        arrows.SetActive(false);
+        wasd.SetActive(true);
     }
 
     IEnumerator ArrowsCompleted()
     {
         tuto2Text.text = "Awesome!";
         yield return new WaitForSeconds(1.0f);
-        arrows.SetActive(false);
+        wasd.SetActive(false);
         tutorialCanvas[1].SetActive(false);
     }
 
@@ -145,6 +153,22 @@ public class Tutorial_Manager : MonoBehaviour
     {
         tutorialCanvas[2].SetActive(true);
         MoveUpTutorial3();
+        initialPos = trailTutorial3.transform.position;
+        stopTrail = false;
+        trailMovement();
+    }
+
+    void trailMovement()
+    {
+        if (!stopTrail)
+        {
+            Camera cam = Camera.main;
+            trailTutorial3.SetActive(true);
+            LeanTween.move(trailTutorial3, initialPos, 1.55f);
+            LeanTween.delayedCall(2.55f, () => trailTutorial3.SetActive(false));
+            trailTutorial3.transform.position = cam.transform.position - new Vector3(0.0f, 1.0f, 0.0f);
+            Invoke("trailMovement", 4.0f);
+        }
     }
 
     public void MoveUpTutorial3()
@@ -162,6 +186,7 @@ public class Tutorial_Manager : MonoBehaviour
     public void DeactivateTutorial3()
     {
         tutorialCanvas[2].SetActive(false);
+        stopTrail = true;
     }
     #endregion
 }
