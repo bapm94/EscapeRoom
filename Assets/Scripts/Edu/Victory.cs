8573dd5c83e6f39f68bd47cf342165ff9c2758c7
penagarciaeduardo <penagarciaeduardo@gmail.com>
2023-03-10 11:32:27 +0000

    VictoryScreen

--- Assets/Scripts/Edu/Victory.cs
+++ Assets/Scripts/Edu/Victory.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Victory : MonoBehaviour
 {
     [SerializeField] GameObject hud;
     [SerializeField] GameObject hud2;
+    [SerializeField] GameObject selected;
 
     private void OnEnable()
     {
         hud.SetActive(false);
         hud2.SetActive(false);
+        LeanTween.delayedCall(2, () => selected.SetActive(true));
     }
 }
